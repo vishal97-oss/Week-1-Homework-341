9f68a6566addd5a74426d1eb2c1205cfc573f296
Vishal Vaid <vai19004@byui.edu>
2024-01-13 17:01:46 -0700

    Week 1 Homework

---
+++ server.js
@@ -0,0 +1,19 @@
+// // express webserver    
+// var express = require("express");
+// var app = express();
+// var server = require('http').createServer(app);
+// var io = require('socket.io')(server);
+// var port = process.env.PORT || 3000;
+
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Vishal Hayli");
+});
+
+const port = 3000;
+
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+});

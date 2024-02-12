a5349c97ceb8261cf6fa8747fffbf68bc296f522
Vishal Vaid <vai19004@byui.edu>
2024-02-12 13:27:14 -0700

    Week4

--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,8 @@ const routes = require('express').Router();
 const lesson1 = require('../control/lesson1');
 const contactController = require('../control/contacts');
 const mongodb = require('../db/connect');
+const swaggerAutogen = require('swagger-autogen')();
+
 
 routes.get('/', lesson1.vishal); // get function is like any other function such as listen, post, put and delete
    // When we click on hompage it says hello 
@@ -17,6 +19,9 @@ routes.get('/contacts/:id', contactController.getSingle )
 routes.post('/contacts', contactController.createContact)
 
 routes.put('/contacts/:id', contactController.updateContact)
+
+// routes.use('/api-docs', swaggerUi.serve);
+// routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 routes.delete('/contacts/:id', contactController.deleteContact)
 

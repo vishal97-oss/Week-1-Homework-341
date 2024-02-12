00498f0b93e17545521a05c1b6a1508c685c23ad
Vishal Vaid <vai19004@byui.edu>
2024-02-12 15:24:43 -0700

    A1

--- swagger.js
+++ swagger.js
@@ -5,17 +5,14 @@ const doc = {
     title: 'My API',
     description: 'Temple API'
   },
-  host: 'https://localhost:3000/',
+  host: 'cse-341-winter2023.onrender.com',
   schemes: ['https']
 };
 
+
+
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./server.js'];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
-
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
